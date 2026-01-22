782afbc5912d7527ea0aa2b7647f248aac912691
unkfex-rgb <unkfex@gmail.com>
2026-01-21 22:43:17 -0300

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <span
+        className="dev"
+        onClick={() =>
+          window.open("https://www.instagram.com/corintia420/", "_blank")
+        }
+      >
+        Dev by Corintia420
+      </span>
+    </footer>
+  );
+}

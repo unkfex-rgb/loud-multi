af75da7bc8f1d0fc64988cf3b4da26b1481148aa
unkfex-rgb <unkfex@gmail.com>
2026-01-21 22:42:55 -0300

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo-area">
+        <img src="/logo.png" alt="LOUD" />
+        <span>LOUD MULTI</span>
+      </div>
+    </header>
+  );
+}

53cf0a4ad095c1242a732ba7408d66e2f0a0714a
unkfex-rgb <unkfex@gmail.com>
2026-01-21 23:32:20 -0300

    Create Player.jsx

---
+++ components/Player.jsx
@@ -0,0 +1,19 @@
+
+"use client";
+
+export default function Player({ streamer, big, height }) {
+  const h = height || (big ? '600px' : '300px');
+
+  return (
+    <div className="player" style={{ height: h }}>
+      <iframe
+        src={`https://player.twitch.tv/?channel=${streamer}&parent=loud-multi.vercel.app`}
+        height="100%"
+        width="100%"
+        frameBorder="0"
+        allowFullScreen
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
+}

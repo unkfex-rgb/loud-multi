c219c7586c14206c8f34ad63d2a7ff5688e76172
unkfex-rgb <unkfex@gmail.com>
2026-01-21 22:54:20 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,21 @@
+"use client";
 
+import { useState } from "react";
 import GridLayout from "../components/GridLayout";
+import FocusLayout from "../components/FocusLayout";
+import AutoLayout from "../components/AutoLayout";
+import ModeMenu from "../components/ModeMenu";
 
 export default function Home() {
+  const [mode, setMode] = useState("grid");
+
   return (
-    <GridLayout />
+    <>
+      <ModeMenu mode={mode} setMode={setMode} />
+
+      {mode === "grid" && <GridLayout />}
+      {mode === "focus" && <FocusLayout />}
+      {mode === "auto" && <AutoLayout />}
+    </>
   );
 }

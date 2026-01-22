161e239471c3695262c9aabe31b11ecd4af236f1
unkfex-rgb <unkfex@gmail.com>
2026-01-21 22:42:26 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <section className="container">
+      <h1 className="title">LOUD MULTI</h1>
+      <p className="subtitle">
+        Stream Hub futurista — Grid, Foco e Auto em breve
+      </p>
+    </section>
+  );
+}

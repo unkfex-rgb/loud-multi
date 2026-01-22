4fc5d0f614887bef693dbd706afb306f9e53cc9b
unkfex-rgb <255659008+unkfex-rgb@users.noreply.github.com>
2026-01-21 23:52:17 -0500

    Fix: Force dark mode for Twitch chat embed

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,14 +1,15 @@
 "use client";
 
-
 export default function Chat({ streamer }) {
   return (
-    <iframe
-      src={`https://www.twitch.tv/embed/${streamer}/chat?parent=loud-multi.vercel.app`}
-      width="100%"
-      height="100%"
-      frameBorder="0"
-      scrolling="no"
-    ></iframe>
+    <div className="chat-container">
+      <iframe
+        src={`https://www.twitch.tv/embed/${streamer}/chat?parent=loud-multi.vercel.app&darkpopout`}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
   );
 }

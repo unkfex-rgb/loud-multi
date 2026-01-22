534ba0deec4cab3fc9c77c34fb441d96d6232983
unkfex-rgb <unkfex@gmail.com>
2026-01-21 22:50:26 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,8 @@
 
+import GridLayout from "../components/GridLayout";
+
 export default function Home() {
   return (
-    <section className="container">
-      <h1 className="title">LOUD MULTI</h1>
-      <p className="subtitle">
-        Stream Hub futurista — Grid, Foco e Auto em breve
-      </p>
-    </section>
+    <GridLayout />
   );
 }
